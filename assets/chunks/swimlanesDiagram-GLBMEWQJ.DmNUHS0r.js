3ddf2e604be19e88d42e1a218b81b0ca5d3dc8d2
AnsGoo <AnsGoo@users.noreply.github.com>
2026-08-30 07:53:10 +0000

    deploy: 06388a839b65ae20258d5dbc059d860f6e9c9837

---
+++ assets/chunks/swimlanesDiagram-GLBMEWQJ.DmNUHS0r.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-JEP3DFND.DGO5y8t2.js";import{_ as a}from"../app.BjQhyfbt.js";import"./chunk-ZICDAICJ.voG52pn0.js";import"./chunk-RTBOCTTP.Cxzkj3wU.js";import"./chunk-HPLX5OYV.9Aha9pcJ.js";import"./chunk-BYAF6FXG.DWHABlCv.js";import"./framework.lAP4D0s8.js";import"./theme.DfHWrblc.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
